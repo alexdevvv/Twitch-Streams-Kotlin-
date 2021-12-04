619025ecef7b7986580d9e024d824b602029082e
Aleksandr Sergeev <alex.dev.sergeev@gmail.com>
2021-12-04 10:54:51 +0300

    V2.3

--- app/src/main/java/com/example/twitchstreamskotlin/presentation/MainActivityVM.kt
+++ app/src/main/java/com/example/twitchstreamskotlin/presentation/MainActivityVM.kt
@@ -1,22 +1,22 @@
 package com.example.twitchstreamskotlin.presentation
 
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.paging.PagedList
 import androidx.paging.RxPagedListBuilder
-import com.example.twitchstreamskotlin.domain.GetFromDBUseCase
 import com.example.twitchstreamskotlin.model.GameData
 import com.example.twitchstreamskotlin.presentation.recycler_view.pagination.GamesFromDbFactory
 import com.example.twitchstreamskotlin.presentation.recycler_view.pagination.TopGamesSourceFactory
-import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.net.UnknownHostException
 
 
 class MainActivityVM(
 
-    val getFromDBUseCase: GetFromDBUseCase,
     val topGamesSourceFactory: TopGamesSourceFactory,
     val gamesFromDbFactory: GamesFromDbFactory
 ) : ViewModel() {
@@ -45,7 +45,12 @@ class MainActivityVM(
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
-                initGamesFromDbFactory()
+                if (it is UnknownHostException) {
+                    initGamesFromDbFactory()
+                    Log.e("Error", it.message!!)
+                } else{
+                    Log.e("Error", it.message!!)
+                }
             }
         )
     }
@@ -64,7 +69,6 @@ class MainActivityVM(
             .subscribe {
                 liveDataModel.postValue(it)
             })
-
     }
 
 }
